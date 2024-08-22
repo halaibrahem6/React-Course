888f9e162494ee3badbc35163dd89db3753f6ad2
hala ibrahim <halamamoun9@gmail.com>
2024-08-22 13:18:34 +0300

    ex5

---
+++ Section3/Exercises/exercise5/App.js
@@ -0,0 +1,24 @@
+export function CourseGoal(props) {
+    return (
+      <li>
+        <h2>{props.title}</h2>
+        <p>{props.description}</p>
+      </li>
+    );
+  }
+  
+  function App() {
+    return (
+      <div id="app" data-testid="app">
+        <h1>Time to Practice</h1>
+        <p>One course, many goals! 🎯</p>
+        <ul>
+          
+          <CourseGoal title="Learn React" description="In-dept"/>
+          <CourseGoal title="Java Script" description="Div-in"/>
+        </ul>
+      </div>
+    );
+  }
+  
+  export default App;
