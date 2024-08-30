fa0e88a717430a95278fde131b32eebf2523d365
hala ibrahim <halamamoun9@gmail.com>
2024-08-30 11:22:33 +0300

    Activate Rematch Button

--- Section4/main/07-tic-tac-toe-starting-project/src/App.jsx
+++ Section4/main/07-tic-tac-toe-starting-project/src/App.jsx
@@ -31,7 +31,7 @@ function App() {
 
   const activePlayer=deriveActivePlayer(gameTurns);
 
-  let gameBoard= initialGameBoard;
+  let gameBoard= [...initialGameBoard.map(array=>[...array])];
 
   for(const turn of gameTurns){ 
     const{square,player}=turn;
@@ -74,6 +74,11 @@ const hasDraw=gameTurns.length===9 && !winner;
   }
   
 
+  function handleRestart(){
+     setGameTurns([]);
+  }
+
+
   return (
     <main>
       <div id="game-container">
@@ -82,7 +87,7 @@ const hasDraw=gameTurns.length===9 && !winner;
          <Player initialName="player2" symbol="O" isActive={activePlayer === 'O'}/>
         </ol>
 
-        {(winner || hasDraw)&& <GameOver winner={winner}/>}
+        {(winner || hasDraw)&& <GameOver winner={winner} onRestart={handleRestart}/>}
         <GameBoard onSelectSquare={handleSelectSquare} board={gameBoard}/>
 
       </div>
