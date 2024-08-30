0d1906b6593181dae881aca3828834fa9641f478
hala ibrahim <halamamoun9@gmail.com>
2024-08-30 17:22:43 +0300

    Getting started with Investment Calculator Web App

---
+++ Section5/Main/01-starting-project/01-starting-project/src/components/Header.jsx
@@ -0,0 +1,11 @@
+
+import logo from '../assets/investment-calculator-logo.png';
+
+export  default function Header(){
+
+    return <header id="header">
+        <img src={logo} alt="Logo showing a Money"/>
+        <h1>Investment Calculater</h1>
+            </header>
+
+}
