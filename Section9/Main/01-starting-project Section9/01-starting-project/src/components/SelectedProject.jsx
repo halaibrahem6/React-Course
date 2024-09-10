e5b7dbba851feee12da5ca0dd968360d6eeebb6d
hala ibrahim <halamamoun9@gmail.com>
2024-09-10 18:43:06 +0300

    Adding project tasks

--- Section9/Main/01-starting-project Section9/01-starting-project/src/components/SelectedProject.jsx
+++ Section9/Main/01-starting-project Section9/01-starting-project/src/components/SelectedProject.jsx
@@ -1,3 +1,5 @@
+import Tasks from './Tasks.jsx'
+
 export default function SelectedProject({project, onDelete}){
     const formattedDate=new Date(project.dueDate).toLocaleDateString('en-US',{
      year:'numeric',
@@ -18,6 +20,6 @@ return (
       <p className="mb-4 text-stone-400">{formattedDate}</p>
       <p className="text-stone-600 whitespace-pre-wrap">{project.description}</p>
     </header>
-    TASK
+    <Tasks />
 </div>);
 }
