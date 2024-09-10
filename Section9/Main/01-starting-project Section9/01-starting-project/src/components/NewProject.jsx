8cc4b0d312bde8fb589f7ad78ac4c45b22789c5b
hala ibrahim <halamamoun9@gmail.com>
2024-09-10 16:54:00 +0300

    Validating user input & showing an error modal via useImperativeHandle and styling

--- Section9/Main/01-starting-project Section9/01-starting-project/src/components/NewProject.jsx
+++ Section9/Main/01-starting-project Section9/01-starting-project/src/components/NewProject.jsx
@@ -1,8 +1,10 @@
 
 import { useRef } from 'react'
 import Input from './Input.jsx'
+import Modal from './Modal.jsx';
 
-export default function NewProject({onAdd}){
+export default function NewProject({onAdd, onCancel}){
+    const modal=useRef();
    const title = useRef();
    const description = useRef();
    const dueDate = useRef();
@@ -13,7 +15,13 @@ export default function NewProject({onAdd}){
     const enteredDescription=description.current.value;
     const enteredDueDate=dueDate.current.value;
 
-    //Validation
+    if(enteredTitle.trim() === '' ||
+     enteredDescription.trim() === '' || 
+     enteredDueDate.trim() === ''
+    ) {
+          modal.current.open();
+          return;
+    }
 
     onAdd({
         title:enteredTitle,
@@ -23,9 +31,15 @@ export default function NewProject({onAdd}){
    }
 
     return (
+        <>
+        <Modal ref={modal} buttonCaption="Okay">
+            <h2 className="text-xl font-bold text-stone-700 my-4">Invalid Input</h2>
+            <p className="text-stone-600 mb-4">Oops ... looks like you forgot to enter a vlue.</p>
+            <p className="text-stone-600 mb-4">Please make sure you provide a valid value for every input field.</p>
+        </Modal>    
         <div className='w-[35rem] mt-16'>
             <menu className='flex items-center justify-end gap-4 my-4'>
-                <li><button className='text-stone-800 hover:text-stone-950'>Cancel</button></li>
+                <li><button className='text-stone-800 hover:text-stone-950' onClick={onCancel}>Cancel</button></li>
                 <li><button onClick={handleSave} className='px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950'>Save</button></li>
             </menu>
             <div>
@@ -34,5 +48,6 @@ export default function NewProject({onAdd}){
                 <Input type="date" ref={dueDate} label="Due Date"/>
             </div>
         </div>
+        </>
     )
 }
