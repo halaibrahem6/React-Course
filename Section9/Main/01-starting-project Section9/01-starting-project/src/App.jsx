1beb2326b5204b1ced21a57af81ec4f1a22d59ee
hala ibrahim <halamamoun9@gmail.com>
2024-09-10 17:50:12 +0300

    Making projects selectable deletable and viewing project details

--- Section9/Main/01-starting-project Section9/01-starting-project/src/App.jsx
+++ Section9/Main/01-starting-project Section9/01-starting-project/src/App.jsx
@@ -2,12 +2,23 @@ import { useState } from "react";
 import NewProject from "./components/NewProject.jsx";
 import NoProjectSelected from "./components/NoProjectSelected.jsx";
 import ProjectsSideBar from "./components/projectsSideBar.jsx";
+import SelectedProject from "./components/SelectedProject.jsx";
 
 function App() {
  const [projectsState,setProjectsState]= useState({
     selectedProjectId:undefined,
     projects:[]
   });
+
+
+  function handleSelectProject(id){
+    setProjectsState(prevState => {
+      return {
+        ...prevState,
+        selectedProjectId:id,
+      };
+    });
+  }
 
 function handleStartAddProject(){
   setProjectsState(prevState => {
@@ -44,7 +55,18 @@ function handleAddProject(projectData){
 });
 }
 
-let content;
+function handleDeleteProject(id){
+  setProjectsState(prevState => {
+    return {
+      ...prevState,
+      selectedProjectId:undefined,
+      projects:prevState.projects.filter( (project) => project.id !== prevState.selectedProjectId ),
+    };
+  });
+}
+
+ const selectedProject=projectsState.projects.find(project => project.id === projectsState.selectedProjectId);
+let content=<SelectedProject project={selectedProject} onDelete={handleDeleteProject}/>;
 if (projectsState.selectedProjectId === null){
   content = <NewProject onAdd={handleAddProject} onCancel={handleCancelAddProject}/>
 }
@@ -53,7 +75,8 @@ else if(projectsState.selectedProjectId === undefined){
 }
   return (
     <main className="h-screen my-8 flex gap-8">
-        <ProjectsSideBar onStartAddProject={handleStartAddProject} projects={projectsState.projects}/>
+        <ProjectsSideBar onStartAddProject={handleStartAddProject} projects={projectsState.projects} onSelectProject={handleSelectProject}/>
+
 {content}
     </main>
   );
